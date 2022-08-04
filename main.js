d9054b59bc62b9560df81c6df644bace66253e3e
Maciej Handke <maciejhandke11@gmail.com>
2022-08-04 23:13:18 +0200

    Add functions for switching active players and holding points

--- main.js
+++ main.js
@@ -1,23 +1,55 @@
 "use strict";
 
-const hold = document.querySelector("#roll-dice-button");
+const roll = document.querySelector("#roll-dice-button");
+const hold = document.querySelector("#hold-button");
 const dice = document.querySelector("#dice-image");
+const playerOneBox = document.querySelector("#player1-box");
+const playerTwoBox = document.querySelector("#player2-box");
+let globalPointsText = document.querySelectorAll(".global-points");
 let currentPointsText = document.querySelectorAll(".current-points");
-let globalPoints = 0;
+let globalPoints = [0, 0];
 let currentPoints = 0;
+let activePlayer = 0;
+
+function switchPlayers() {
+  dice.style.display = "none";
+  if (activePlayer === 0) {
+    activePlayer = 1;
+    playerOneBox.classList.toggle("active");
+    playerTwoBox.classList.toggle("active");
+  } else {
+    activePlayer = 0;
+    playerOneBox.classList.toggle("active");
+    playerTwoBox.classList.toggle("active");
+  }
+}
 
 function rollDice() {
+  dice.style.display = "block";
   const dicePoints = Math.trunc(Math.random() * 6) + 1;
-  console.log(dicePoints);
   dice.classList.remove("hidden");
   dice.src = `images/dice-${dicePoints}.svg`;
   currentPoints += dicePoints;
   if (dicePoints === 1) {
-    currentPointsText.textContent = 0;
+    currentPointsText[activePlayer].textContent = 0;
     currentPoints = 0;
+    switchPlayers();
   } else {
-    currentPointsText[0].textContent = currentPoints;
+    currentPointsText[activePlayer].textContent = currentPoints;
+  }
+  console.log(`Global: ${globalPoints}
+Current: ${currentPoints}`);
+}
+
+function holdPoints() {
+  if (currentPoints !== 0) {
+    globalPoints[activePlayer] += currentPoints;
+    globalPointsText[activePlayer].textContent = globalPoints[activePlayer];
+    currentPointsText[activePlayer].textContent = 0;
+    switchPlayers();
+    currentPoints = 0;
   }
 }
 
-hold.addEventListener("click", rollDice);
+roll.addEventListener("click", rollDice);
+hold.addEventListener("click", holdPoints);
